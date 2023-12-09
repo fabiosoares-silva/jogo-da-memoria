1920b7fdfdfdc69754de9f5f2b2553da7c0eb364
fabiosoares-silva <fabiosores.soares@gmail.com>
2023-12-09 19:00:51 -0300

    Incluir funcionalidades javascript e ajustes css

---
+++ src/scripts/engine.js
@@ -0,0 +1,42 @@
+const emojis = ['😀', '😀', '🎭', '🎭', '🛒', '🛒', '👨🏽‍🦲', '👨🏽‍🦲', '🥓', '🥓', '❤', '❤', '🎂', '🎂', '🙌', '🙌'];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector('.game').appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add('boxOpen');
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+
+    console.log(openCards);
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add('boxMatch');
+        openCards[1].classList.add('boxMatch');
+    } else {
+        openCards[0].classList.remove('boxOpen');
+        openCards[1].classList.remove('boxOpen');
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll('.boxMatch').length === emojis.length) {
+        alert('Fim de Jogo! Parabéns! Você lembrou onde todas as cartas estavam!');
+    }
+}
